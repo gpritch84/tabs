47e9d37345402324d2b1a5ba427d7cfb91844ee3
Greg <gpritch84@yahoo.com>
2020-11-02 16:24:49 -0600

    Tabs project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TabNav from './components/TabNav';
+import Tab from './components/Tab';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: 'Tab 1'
+    }
+  }
+  setSelected = (tab) => {
+    this.setState({ selected: tab });
+  }
+  render() {
+    return (
+      <div className="App mt-4">
+        <TabNav tabs={['Tab 1', 'Tab 2', 'Tab 3']} selected={this.state.selected} setSelected={this.setSelected}>
+          <Tab isSelected={this.state.selected === 'Tab 1'}>
+            <p>Text for tab 1</p>
+          </Tab>
+          <Tab isSelected={this.state.selected === 'Tab 2'}>
+            <h1>Text for tab 2</h1>
+          </Tab>
+          <Tab isSelected={this.state.selected === 'Tab 3'}>
+            <ul>
+              <li>Test 1</li>
+              <li>Test 2</li>
+              <li>Test 3</li>
+            </ul>
+          </Tab>
+        </TabNav>
+      </div>
+    );
+  }
 }
-
 export default App;
